c60424d7466bfc1267faf566d0254f9bb207fb45
Yavuz Semiz <yavuz.s25@gmail.com>
2019-11-10 21:34:58 +0100

    Day 5 test

--- src/test/java/com/cybertek/tests/Day_5_Xpath/AmazonTest.java
+++ src/test/java/com/cybertek/tests/Day_5_Xpath/AmazonTest.java
@@ -6,13 +6,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class AmazonTest {
-    public static void main(String[] args) {
-        WebDriver driver= WebDriverFactory.getDriver("chrome");
+    public static void main(String[] args) throws InterruptedException {
+       /** WebDriver driver= WebDriverFactory.getDriver("chrome");
         driver.get("https://www.amazon.com");
         WebElement search = driver.findElement(By.xpath("//*[@type = 'text']"));
          search.sendKeys("Selenium");
 
         WebElement go = driver.findElement(By.xpath("//*[@type = 'submit']"));
-        go.click();
-    }
+        go.click();*/
+
+
+
+            /**
+             * navigate to amazon.com
+             * type selenium in the searchbox
+             * click search button
+             */
+
+
+                WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+                //driver.manage().window().maximize();
+
+                driver.get("https://www.amazon.com");
+
+                //locate searchbox
+                WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+                //type selenium
+                searchBox.sendKeys("Selenium");
+
+
+                //locate searchButton
+                WebElement searchButton = driver.findElement(By.xpath("//input[@value='Go']"));
+
+                searchButton.click();
+                String resultMessage=driver.findElement(By.xpath("//span[contains(text(),'results for')]"));
+                System.out.println( resultMessage);
+                Thread.sleep(3000);
+                driver.quit();
+
+            }
 }
